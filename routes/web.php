3d3770290857523c8b1083ca6609c34d849e8073
kenziehh <ghaisani.nurani@gmail.com>
2025-04-10 03:23:21 +0700

    `feat`: add update and delete photo profile function with route

--- routes/web.php
+++ routes/web.php
@@ -22,6 +22,8 @@ Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+    Route::post('/profile/photo', [ProfileController::class, 'updatePhoto'])->name('profile.updatePhoto');
+    Route::delete('/profile/photo', [ProfileController::class, 'deletePhoto'])->name('profile.deletePhoto');
 });
 
-require __DIR__.'/auth.php';
+require __DIR__ . '/auth.php';
