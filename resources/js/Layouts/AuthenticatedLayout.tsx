1a088cde571bdda56c057eba14c492975eb76c65
kenziehh <ghaisani.nurani@gmail.com>
2025-04-10 03:25:08 +0700

    `feat` : add profile user page with edit profile, edit photo profile, and delete photo profile

--- resources/js/Layouts/AuthenticatedLayout.tsx
+++ resources/js/Layouts/AuthenticatedLayout.tsx
@@ -15,9 +15,9 @@ export default function Authenticated({
 
 	return (
 		<SidebarProvider>
-			<div className="min-h-screen bg-[#F2F2F2]">
+			<div className="min-h-screen bg-[#F2F2F2] max-w-screen overflow-hidden">
 				<AppSidebar />
-				<main className="text-black pl-0 md:pl-[30vh] xl:pl-[30vh] 2xl:pl-[40vh] min-h-screen flex-1 w-screen">
+				<main className="text-black pl-0 md:pl-[35vh] xl:pl-[30vh] 2xl:pl-[40vh] min-h-screen flex-1 w-screen">
 					<div className="p-6 md:p-8 flex flex-col gap-">
 						{/* <div className="w-full py-8">
 							<h2 className="text-h4 text-black">
