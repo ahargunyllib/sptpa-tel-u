efe136ed68b96567a8ed8efef875d6a01eafeb51
kenziehh <ghaisani.nurani@gmail.com>
2025-04-09 09:35:19 +0700

    `feat` : slicing layout dashboard and conditional sidebar menu integrated with user role

---
+++ resources/js/Components/sidebar/index.tsx
@@ -0,0 +1,179 @@
+"use client";
+
+import { Button } from "@/components/ui/button";
+import { useIsMobile } from "@/hooks/use-mobile";
+import { cn } from "@/lib/utils";
+import type { User } from "@/types";
+
+import {
+	type NavSection,
+	arsipSections,
+	kaurPenilaianSections,
+	sdmPenilaianSections,
+	tpaPenilaianSections,
+	wadekPenilaianSections,
+} from "@/components/sidebar/sidebar";
+import { Link, usePage } from "@inertiajs/react";
+import { ClipboardList, Folder, Menu, X } from "lucide-react";
+import { useEffect, useState } from "react";
+
+export function AppSidebar() {
+	const user = usePage().props.auth.user as User;
+	const [isOpen, setIsOpen] = useState(true);
+	const [activeMenu, setActiveMenu] = useState<"penilaian" | "e-archive">(
+		"penilaian",
+	);
+	const isMobile = useIsMobile();
+	const pathName = window.location.pathname;
+	const [menus, setMenus] = useState<NavSection[]>(tpaPenilaianSections);
+
+	useEffect(() => {
+		if (isMobile) {
+			setIsOpen(false);
+		} else {
+			setIsOpen(true);
+		}
+	}, [isMobile]);
+
+	useEffect(() => {
+		const role = user.role;
+		if (activeMenu === "e-archive") {
+			setMenus(arsipSections);
+		} else if (role === "tpa") {
+			setMenus(tpaPenilaianSections);
+		} else if (role === "kaur") {
+			setMenus(kaurPenilaianSections);
+		} else if (role === "sdm") {
+			setMenus(sdmPenilaianSections);
+		} else if (role === "wadek") {
+			setMenus(wadekPenilaianSections);
+		}
+	}, [activeMenu, user.role]);
+
+	return (
+		<>
+			{/* Mobile overlay */}
+			{isMobile && isOpen && (
+				// biome-ignore lint/a11y/useKeyWithClickEvents: <explanation>
+				<div
+					className="fixed inset-0 bg-white z-40"
+					onClick={() => setIsOpen(false)}
+				/>
+			)}
+
+			{/* Toggle button for mobile */}
+			{isMobile && (
+				<Button
+					variant="ghost"
+					size="icon"
+					className="fixed top-4 left-4 z-[999]"
+					onClick={() => setIsOpen(!isOpen)}
+				>
+					{isOpen ? <X className="h-5 w-5" /> : <Menu className="h-5 w-5" />}
+					<span className="sr-only">Toggle menu</span>
+				</Button>
+			)}
+
+			{/* Sidebar */}
+			<aside
+				className={cn(
+					"py-12 fixed inset-y-0 left-0 z-50 flex xl:w-[30vh] 2xl:w-[40vh] flex-col bg-white text-[#475467] transition-transform duration-300 px-4 w-full md:w-auto ",
+					isMobile && !isOpen && "-translate-x-full",
+				)}
+			>
+				<div className="flex flex-col h-full gap-8">
+					{/* Logo */}
+					<div className="p-6 text-center">
+						<h1 className="font-bold text-xl text-primary-60">SPTPA</h1>
+					</div>
+
+					<div className="grid grid-cols-2">
+						{/* biome-ignore lint/a11y/useKeyWithClickEvents: <explanation> */}
+						<div
+							className={cn(
+								"cursor-pointer rounded-t-xl border-b-2 py-3 px-5 border-transparent",
+								activeMenu === "penilaian" && "bg-[#FFF0F1] border-b-danger-80",
+							)}
+							onClick={() => setActiveMenu("penilaian")}
+						>
+							<div className="flex justify-center items-center flex-col gap-2">
+								<ClipboardList
+									className={cn(
+										activeMenu === "penilaian"
+											? "text-danger-80 "
+											: "text-light-100",
+										"w-4 h-4",
+									)}
+								/>
+								<span
+									className={`${activeMenu === "penilaian" ? "text-danger-80 " : "text-light-100"}`}
+								>
+									Penilaian
+								</span>
+							</div>
+						</div>
+						{/* biome-ignore lint/a11y/useKeyWithClickEvents: <explanation> */}
+						<div
+							className={cn(
+								"cursor-pointer rounded-t-xl border-b-2 border-transparent py-3 px-5",
+								activeMenu === "e-archive" &&
+									"bg-[#FFF0F1] border-b-danger-80 ",
+							)}
+							onClick={() => setActiveMenu("e-archive")}
+						>
+							<div className="flex justify-center items-center flex-col gap-2">
+								<Folder
+									className={cn(
+										activeMenu === "e-archive"
+											? "text-danger-80 "
+											: "text-light-100",
+										"w-4 h-4",
+									)}
+								/>
+								<span
+									className={`${activeMenu === "e-archive" ? "text-danger-80 " : "text-light-100"}`}
+								>
+									E-Arsip
+								</span>
+							</div>
+						</div>
+					</div>
+
+					{/* Navigation */}
+					<nav className="flex-1 px-4 space-y-8">
+						{menus.map((section) => (
+							<div key={section.title} className="space-y-2">
+								<h2 className="text-xs tracking-wider text-dark-100 font-semibold px-2">
+									{section.title}
+								</h2>
+								<ul className="space-y-2">
+									{section.items.map((item) => (
+										<li key={item.title}>
+											<Link
+												href={item.href}
+												method={item.href === "/logout" ? "post" : undefined}
+												className={cn(
+													"flex items-center gap-6 px-6 py-4 rounded-lg text-body-lg xl:text-h6 transition-colors",
+													pathName === item.href ? "bg-primary-60" : "bg",
+												)}
+											>
+												<item.icon
+													className={`${item.href === pathName ? "text-white" : "text-dark-60"} w-5 h-5`}
+												/>
+												<span
+													className={`${item.href === pathName ? "text-white" : "text-dark-60"}`}
+												>
+													{item.title}
+												</span>
+											</Link>
+										</li>
+									))}
+								</ul>
+							</div>
+						))}
+					</nav>
+				</div>
+			</aside>
+		</>
+	);
+}
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,124 +1,125 @@
-import defaultTheme from "tailwindcss/defaultTheme";
 import forms from "@tailwindcss/forms";
+import defaultTheme from "tailwindcss/defaultTheme";
 
 /** @type {import('tailwindcss').Config} */
 export default {
-    darkMode: ["class"],
-    content: [
-        "./vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php",
-        "./storage/framework/views/*.php",
-        "./resources/views/**/*.blade.php",
-        "./resources/js/**/*.tsx",
-    ],
+	darkMode: ["class"],
+	content: [
+		"./vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php",
+		"./storage/framework/views/*.php",
+		"./resources/views/**/*.blade.php",
+		"./resources/js/**/*.tsx",
+	],
 
-    theme: {
-        extend: {
-            fontFamily: {
-                sans: ["Figtree", ...defaultTheme.fontFamily.sans],
-            },
-            borderRadius: {
-                lg: "var(--radius)",
-                md: "calc(var(--radius) - 2px)",
-                sm: "calc(var(--radius) - 4px)",
-            },
-            colors: {
-                // Light Colors (White)
-                light: {
-                    100: "#848484",
-                    80: "#AEAEA6",
-                    60: "#CECECE",
-                    40: "#F5F4F4",
-                    20: "#FEF4E8",
-                    DEFAULT: "#F5F4F4",
-                },
+	theme: {
+		extend: {
+			fontFamily: {
+				sans: ["Figtree", ...defaultTheme.fontFamily.sans],
+			},
+			borderRadius: {
+				lg: "var(--radius)",
+				md: "calc(var(--radius) - 2px)",
+				sm: "calc(var(--radius) - 4px)",
+			},
+			colors: {
+				light: {
+					20: "#FEF4E8",
+					40: "#F5F4F4",
+					60: "#CECECE",
+					80: "#AEAEA6",
+					100: "#848484",
+					DEFAULT: "#F5F4F4",
+				},
+				dark: {
+					20: "#737373",
+					40: "#4F4F4F",
+					60: "#404040",
+					80: "#262626",
+					100: "#000000",
+					DEFAULT: "#000000",
+				},
+				danger: {
+					20: "#FFE5E5",
+					40: "#FF9999",
+					60: "#FF666A",
+					80: "#CC0100",
+					100: "#990000",
+					DEFAULT: "#990000",
+				},
+				warning: {
+					20: "#FFF8E5",
+					40: "#FFE599",
+					60: "#FFD766",
+					80: "#FFB000",
+					100: "#CB9701",
+					DEFAULT: "#CB9701",
+				},
+				info: {
+					20: "#E6EFF6",
+					40: "#8FB4D9",
+					60: "#006BD2",
+					80: "#0138B1",
+					100: "#233852",
+					DEFAULT: "#233852",
+				},
+				background: "hsl(var(--background))",
+				foreground: "hsl(var(--foreground))",
+				card: {
+					DEFAULT: "hsl(var(--card))",
+					foreground: "hsl(var(--card-foreground))",
+				},
+				popover: {
+					DEFAULT: "hsl(var(--popover))",
+					foreground: "hsl(var(--popover-foreground))",
+				},
+				primary: {
+					20: "#EFF9F5",
+					40: "#0BD0C5",
+					60: "#328466",
+					80: "#035343",
+					100: "#143529",
+					DEFAULT: "hsl(var(--primary))",
+					foreground: "hsl(var(--primary-foreground))",
+				},
+				secondary: {
+					DEFAULT: "hsl(var(--secondary))",
+					foreground: "hsl(var(--secondary-foreground))",
+				},
+				muted: {
+					DEFAULT: "hsl(var(--muted))",
+					foreground: "hsl(var(--muted-foreground))",
+				},
+				accent: {
+					DEFAULT: "hsl(var(--accent))",
+					foreground: "hsl(var(--accent-foreground))",
+				},
+				destructive: {
+					DEFAULT: "hsl(var(--destructive))",
+					foreground: "hsl(var(--destructive-foreground))",
+				},
+				border: "hsl(var(--border))",
+				input: "hsl(var(--input))",
+				ring: "hsl(var(--ring))",
+				chart: {
+					1: "hsl(var(--chart-1))",
+					2: "hsl(var(--chart-2))",
+					3: "hsl(var(--chart-3))",
+					4: "hsl(var(--chart-4))",
+					5: "hsl(var(--chart-5))",
+				},
+				sidebar: {
+					DEFAULT: "hsl(var(--sidebar-background))",
+					foreground: "hsl(var(--sidebar-foreground))",
+					primary: "hsl(var(--sidebar-primary))",
+					"primary-foreground": "hsl(var(--sidebar-primary-foreground))",
+					accent: "hsl(var(--sidebar-accent))",
+					"accent-foreground": "hsl(var(--sidebar-accent-foreground))",
+					border: "hsl(var(--sidebar-border))",
+					ring: "hsl(var(--sidebar-ring))",
+				},
+			},
+		},
+	},
 
-                // Dark Colors (Black)
-                dark: {
-                    100: "#000000",
-                    80: "#262626",
-                    60: "#404040",
-                    40: "#4F4F4F",
-                    20: "#737373",
-                    DEFAULT: "#000000",
-                },
-
-                // Danger Colors (Red)
-                danger: {
-                    100: "#990000",
-                    80: "#CC0100",
-                    60: "#FF666A",
-                    40: "#FF9999",
-                    20: "#FFE5E5",
-                    DEFAULT: "#990000",
-                },
-
-                // Warning Colors (Yellow)
-                warning: {
-                    100: "#CB9701",
-                    80: "#FFB000",
-                    60: "#FFD766",
-                    40: "#FFE599",
-                    20: "#FFF8E5",
-                    DEFAULT: "#CB9701",
-                },
-
-                // Info Colors (Blue)
-                info: {
-                    100: "#233852",
-                    80: "#0138B1",
-                    60: "#006BD2",
-                    40: "#8FB4D9",
-                    20: "#E6EFF6",
-                    DEFAULT: "#233852",
-                },
-                background: "hsl(var(--background))",
-                foreground: "hsl(var(--foreground))",
-                card: {
-                    DEFAULT: "hsl(var(--card))",
-                    foreground: "hsl(var(--card-foreground))",
-                },
-                popover: {
-                    DEFAULT: "hsl(var(--popover))",
-                    foreground: "hsl(var(--popover-foreground))",
-                },
-                primary: {
-                    DEFAULT: "hsl(var(--primary))",
-                    foreground: "hsl(var(--primary-foreground))",
-                    100: "#143529",
-                    80: "#035343",
-                    60: "#328466",
-                    40: "#0BD0C5",
-                    20: "#EFF9F5",
-                },
-                secondary: {
-                    DEFAULT: "hsl(var(--secondary))",
-                    foreground: "hsl(var(--secondary-foreground))",
-                },
-                muted: {
-                    DEFAULT: "hsl(var(--muted))",
-                    foreground: "hsl(var(--muted-foreground))",
-                },
-                accent: {
-                    DEFAULT: "hsl(var(--accent))",
-                    foreground: "hsl(var(--accent-foreground))",
-                },
-                destructive: {
-                    DEFAULT: "hsl(var(--destructive))",
-                    foreground: "hsl(var(--destructive-foreground))",
-                },
-                border: "hsl(var(--border))",
-                input: "hsl(var(--input))",
-                ring: "hsl(var(--ring))",
-                chart: {
-                    1: "hsl(var(--chart-1))",
-                    2: "hsl(var(--chart-2))",
-                    3: "hsl(var(--chart-3))",
-                    4: "hsl(var(--chart-4))",
-                    5: "hsl(var(--chart-5))",
-                },
-            },
-        },
-    },
-
-    plugins: [forms, require("tailwindcss-animate")],
+	plugins: [forms, require("tailwindcss-animate")],
 };
