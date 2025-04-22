faab7b6e603392d77d1c44e6eecbe27f433283ea
ahargunyllib <billy.bpm03@gmail.com>
2025-04-22 15:48:04 +0700

    feat: enhance WorkTarget and WorkTargetValue models with relationships and fillable properties

--- app/Models/WorkTarget.php
+++ app/Models/WorkTarget.php
@@ -2,9 +2,31 @@
 
 namespace App\Models;
 
+use App\Traits\HasUlid;
 use Illuminate\Database\Eloquent\Model;
 
 class WorkTarget extends Model
 {
-    //
+    use HasUlid;
+
+    protected $fillable = [
+        'name',
+        'unit',
+        'comparator',
+        'first_quarter_target',
+        'second_quarter_target',
+        'third_quarter_target',
+        'fourth_quarter_target',
+        'user_id',
+    ];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function workTargetValues()
+    {
+        return $this->hasMany(WorkTargetValue::class);
+    }
 }
--- app/Models/WorkTargetValue.php
+++ app/Models/WorkTargetValue.php
@@ -2,9 +2,20 @@
 
 namespace App\Models;
 
+use App\Traits\HasUlid;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class WorkTargetValue extends Model
 {
-    //
+  use HasUlid;
+  public function user(): BelongsTo
+  {
+    return $this->belongsTo(User::class);
+  }
+
+  public function workTarget(): BelongsTo
+  {
+    return $this->belongsTo(WorkTarget::class);
+  }
 }
