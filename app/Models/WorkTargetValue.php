73069ee6649a00ef91d0cf5275afdb3dffb2359b
ahargunyllib <billy.bpm03@gmail.com>
2025-04-23 11:34:38 +0700

    feat: add fillable properties to WorkTargetValue model for improved mass assignment

--- app/Models/WorkTargetValue.php
+++ app/Models/WorkTargetValue.php
@@ -9,6 +9,21 @@ use Illuminate\Database\Eloquent\Relations\BelongsTo;
 class WorkTargetValue extends Model
 {
   use HasUlid;
+
+  protected $fillable = [
+    'user_id',
+    'work_target_id',
+    'first_quarter_value',
+    'second_quarter_value',
+    'third_quarter_value',
+    'fourth_quarter_value',
+    'category',
+    'first_quarter_score',
+    'second_quarter_score',
+    'third_quarter_score',
+    'fourth_quarter_score',
+  ];
+
   public function user(): BelongsTo
   {
     return $this->belongsTo(User::class);
