e4d13d4bda55578c7f106bb46fd669fbfc85342a
ahargunyllib <billy.bpm03@gmail.com>
2025-04-24 15:17:37 +0700

    feat: implement HasUlid trait and define fillable attributes for UserAttitudeEvaluation and UserFeedback models

--- app/Models/UserFeedback.php
+++ app/Models/UserFeedback.php
@@ -2,9 +2,20 @@
 
 namespace App\Models;
 
+use App\Traits\HasUlid;
 use Illuminate\Database\Eloquent\Model;
 
 class UserFeedback extends Model
 {
-    //
+    use HasUlid;
+    protected $fillable = [
+        'user_id',
+        'kaur_feedback',
+        'wadek_feedback',
+    ];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
 }
