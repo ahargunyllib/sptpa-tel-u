564a8902c97388d6da7c9e904186b4d1792553dd
kenziehh <ghaisani.nurani@gmail.com>
2025-05-22 23:24:38 +0700

    chore: delete unused seeder

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -2,10 +2,8 @@
 
 namespace Database\Seeders;
 
-use App\Models\Tag;
-use App\Models\TagWeeklyReport;
+
 use App\Models\User;
-use App\Models\WeeklyReport;
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\Hash;
 use App\Traits\Log;
@@ -21,8 +19,7 @@ class UserSeeder extends Seeder
     public function run(): void
     {
         $roles = ['tpa', 'kaur', 'wadek', 'sdm'];
-        $tags = [];
-        $tpaWeeklyReports = [];
+       
 
         foreach ($roles as $role) {
             for ($i = 1; $i <= 2; $i++) {
@@ -37,31 +34,10 @@ class UserSeeder extends Seeder
                     'password' => Hash::make('password123')
                 ]);
 
-                // Buat Tag dan simpan ke array
-                $tag = Tag::create([
-                    'name' => "Tag $i",
-                ]);
-                $tags[] = $tag;
-
-                if ($role === 'tpa') {
-                    // Buat WeeklyReport dan simpan ke array
-                    $weeklyReport = WeeklyReport::create([
-                        'content' => "Billy itu orangnya rame banget, selalu jadi pusat perhatian di tongkrongan. Dia punya gaya ngomong yang nyablak tapi bikin ketawa, kadang sok tahu tapi justru itu yang bikin obrolan makin hidup. Apa aja dijadiin bahan becandaan, bahkan hal-hal receh bisa dia bawa jadi topik seru. Tapi di balik kelakuannya yang heboh, dia sebenarnya peka juga sama temen-temennya—selalu ada kalau dibutuhin. Pokoknya kalau ada Billy, suasana dijamin gak bakal sepi!",
-                        'user_id' => $user->id,
-                    ]);
-                    $tpaWeeklyReports[] = $weeklyReport;
-                }
+               
             }
         }
 
-        // Mapping semua tag ke setiap weekly report TPA
-        foreach ($tpaWeeklyReports as $report) {
-            foreach ($tags as $tag) {
-                TagWeeklyReport::create([
-                    'tag_id' => $tag->id,
-                    'weekly_report_id' => $report->id,
-                ]);
-            }
-        }
+       
     }
 }
