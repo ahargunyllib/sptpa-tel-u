c8cd4ca19d765f3744d82b60d0937487730144cd
kenziehh <ghaisani.nurani@gmail.com>
2025-05-23 20:07:14 +0700

    feat: add not null and cascade constrain

--- database/migrations/2025_05_23_095055_create_work_reports_table.php
+++ database/migrations/2025_05_23_095055_create_work_reports_table.php
@@ -13,9 +13,9 @@ return new class extends Migration
     {
         Schema::create('work_reports', function (Blueprint $table) {
             $table->ulid('id')->primary();
-            $table->foreignUlid('work_target_id')->constrained('work_targets');
-            $table->foreignUuid('creator_id')->constrained('users');
-            $table->text('content');
+            $table->foreignUlid('work_target_id')->constrained('work_targets')->nullable(false)->onDelete('cascade');
+            $table->foreignUuid('creator_id')->constrained('users')->nullable(false);
+            $table->text('content')->nullable(false);;
             $table->timestamps();
         });
     }
