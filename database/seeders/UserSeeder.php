e47a52d590beaa11e41334058f5fde4a784793d0
ahargunyllib <billy.bpm03@gmail.com>
2025-05-25 20:48:05 +0700

    refactor: simplify user creation in UserSeeder by removing nested loops

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -21,21 +21,19 @@ class UserSeeder extends Seeder
         $roles = ['staf', 'kaur', 'wadek1', 'wadek2', 'sdm'];
         $divisions = ['academic_service', 'laboratory', 'secretary', 'student_affair', 'finance_logistic_resource'];
 
-        foreach ($roles as $role) {
-            foreach ($divisions as $division) {
-                for ($i = 1; $i <= 2; $i++) {
-                    $user = User::create([
-                        'name' => strtoupper($role) . ' ' . ucwords(str_replace('_', ' ', $division)) . " $i",
-                        'nip' => rand(10000000, 99999999),
-                        'email' => "$role.$division$i@gmail.com",
-                        'location' => "Bandung",
-                        'division' => $division,
-                        'photo_profile' => null,
-                        'role' => $role,
-                        'password' => Hash::make('password123')
-                    ]);
-                }
-            }
-        }
+        $role = $roles[0];
+        $division = $divisions[4];
+        $i = 1;
+
+        User::create([
+            'name' => strtoupper($role) . ' ' . ucwords(str_replace('_', ' ', $division)) . " $i",
+            'nip' => rand(10000000, 99999999),
+            'email' => "$role.$division$i@gmail.com",
+            'location' => "Bandung",
+            'division' => $division,
+            'photo_profile' => null,
+            'role' => $role,
+            'password' => Hash::make('password123')
+        ]);
     }
 }
