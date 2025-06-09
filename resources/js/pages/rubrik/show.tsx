9f659db068cca91936b118f2b90872669c8c87ae
kenziehh <ghaisani.nurani@gmail.com>
2025-06-10 06:44:07 +0700

    feat: redirect file when show panduan and rubrik, change word rubrikasi to rubrik

--- resources/js/pages/rubrik/show.tsx
+++ resources/js/pages/rubrik/show.tsx
@@ -1,30 +1,39 @@
 import DashboardLayout from "@/layouts/dashboard-layout";
+import { useEffect } from "react";
 
 export default function RubrikasiShowPage({
-	rubrikasi,
+    rubrikasi,
 }: {
-	rubrikasi: string;
+    rubrikasi: string;
 }) {
-	return (
-		<DashboardLayout header="Arsip">
-			<section className="bg-white rounded-md px-6 py-4 flex flex-col gap-4">
-				<div>File Rubrikasi</div>
-				<span>
-					File saat ini{" "}
-					{rubrikasi ? (
-						<a
-							className="text-blue-400 underline"
-							href={rubrikasi}
-							target="_blank"
-							rel="noopener noreferrer"
-						>
-							Link File saat ini
-						</a>
-					) : (
-						<b>Tidak ada file yang diupload.</b>
-					)}
-				</span>
-			</section>
-		</DashboardLayout>
-	);
+    useEffect(() => {
+        if (rubrikasi) {
+            window.location.assign(rubrikasi);
+        }
+    }, []);
+
+    return (
+        !rubrikasi && (
+            <DashboardLayout header="Arsip">
+                <section className="bg-white rounded-md px-6 py-4 flex flex-col gap-4">
+                    <div>File Rubrik</div>
+                    <span>
+                        File saat ini{" "}
+                        {rubrikasi ? (
+                            <a
+                                className="text-blue-400 underline"
+                                href={rubrikasi}
+                                target="_blank"
+                                rel="noopener noreferrer"
+                            >
+                                Link File saat ini
+                            </a>
+                        ) : (
+                            <b>Tidak ada file yang diupload.</b>
+                        )}
+                    </span>
+                </section>
+            </DashboardLayout>
+        )
+    );
 }
